88a0748bc0e630bcfe472f895de83a2c7aa3185a
Deepanshu-Ranjan <dranjan@aisplstore.com>
2022-10-11 23:19:11 +0530

    commit

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/page1', function () {
+Route::get('/page1/page', function () {
     return view('page1');
 });
